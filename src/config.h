e0baeede6ccd88146f4a8a91633e631ba7c6e59c
Terr <megakek@gmail.com>
2017-05-15 19:26:12 +0200

    Use different color, instead of foreground, for cursor

--- src/config.h
+++ src/config.h
@@ -89,7 +89,7 @@ static char *colorname[] = {
 /* fg, bg, cursor, reverse cursor (references colorname indexes) */
 static unsigned int defaultfg = 256;
 static unsigned int defaultbg = 257;
-static unsigned int defaultcs = 256;
+static unsigned int defaultcs = 34;
 static unsigned int defaultrcs = 257;
 
 /* 2 4 6 7: █ _ | ☃ */
